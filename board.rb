8412cca76d6c52022d04661b189549f9e0ac9a2b
App Academy Student <student@appacademy.io>
2016-07-19 16:30:06 -0700

    completed refactoring of checkmate, now about to start refactoring of singleton

--- board.rb
+++ board.rb
@@ -14,49 +14,44 @@ class Board
   end
 
 
+
   def populate_board
-    self[[0,1]] = SlidingPiece.new(:white,[0,1],self,:rook)
-    self[[0,0]] = SlidingPiece.new(:white,[0,0],self,:rook)
-    self[[0,3]] = SteppingPiece.new(:white,[0,3],self,:king)
-    self[[7,0]] = SteppingPiece.new(:black,[7,0],self,:king)
+    self[[0,0]] = SlidingPiece.new(:black,[0,0],self,:rook)
+    self[[0,1]] = SteppingPiece.new(:black,[0,1],self,:knight)
+    self[[0,2]] = SlidingPiece.new(:black,[0,2],self,:bishop)
+    self[[0,3]] = SlidingPiece.new(:black,[0,3],self,:queen)
+    self[[0,4]] = SteppingPiece.new(:black,[0,4],self,:king)
+    self[[0,5]] = SlidingPiece.new(:black,[0,5],self,:bishop)
+    self[[0,6]] = SteppingPiece.new(:black,[0,6],self,:knight)
+    self[[0,7]] = SlidingPiece.new(:black,[0,7],self,:rook)
+    self[[1,0]] = Pawn.new(:black,[1,0],self,:pawn)
+    self[[1,1]] = Pawn.new(:black,[1,1],self,:pawn)
+    self[[1,2]] = Pawn.new(:black,[1,2],self,:pawn)
+    self[[1,3]] = Pawn.new(:black,[1,3],self,:pawn)
+    self[[1,4]] = Pawn.new(:black,[1,4],self,:pawn)
+    self[[1,5]] = Pawn.new(:black,[1,5],self,:pawn)
+    self[[1,6]] = Pawn.new(:black,[1,6],self,:pawn)
+    self[[1,7]] = Pawn.new(:black,[1,7],self,:pawn)
+
+    self[[7,0]] = SlidingPiece.new(:white,[7,0],self,:rook)
+    self[[7,1]] = SteppingPiece.new(:white,[7,1],self,:knight)
+    self[[7,2]] = SlidingPiece.new(:white,[7,2],self,:bishop)
+    self[[7,3]] = SlidingPiece.new(:white,[7,3],self,:queen)
+    self[[7,4]] = SteppingPiece.new(:white,[7,4],self,:king)
+    self[[7,5]] = SlidingPiece.new(:white,[7,5],self,:bishop)
+    self[[7,6]] = SteppingPiece.new(:white,[7,6],self,:knight)
+    self[[7,7]] = SlidingPiece.new(:white,[7,7],self,:rook)
+    self[[6,0]] = Pawn.new(:white,[6,0],self,:pawn)
+    self[[6,1]] = Pawn.new(:white,[6,1],self,:pawn)
+    self[[6,2]] = Pawn.new(:white,[6,2],self,:pawn)
+    self[[6,3]] = Pawn.new(:white,[6,3],self,:pawn)
+    self[[6,4]] = Pawn.new(:white,[6,4],self,:pawn)
+    self[[6,5]] = Pawn.new(:white,[6,5],self,:pawn)
+    self[[6,6]] = Pawn.new(:white,[6,6],self,:pawn)
+    self[[6,7]] = Pawn.new(:white,[6,7],self,:pawn)
+
+
   end
-  # def populate_board
-  #   self[[0,0]] = SlidingPiece.new(:black,[0,0],self,:rook)
-  #   self[[0,1]] = SteppingPiece.new(:black,[0,1],self,:knight)
-  #   self[[0,2]] = SlidingPiece.new(:black,[0,2],self,:bishop)
-  #   self[[0,3]] = SlidingPiece.new(:black,[0,3],self,:queen)
-  #   self[[0,4]] = SteppingPiece.new(:black,[0,4],self,:king)
-  #   self[[0,5]] = SlidingPiece.new(:black,[0,5],self,:bishop)
-  #   self[[0,6]] = SteppingPiece.new(:black,[0,6],self,:knight)
-  #   self[[0,7]] = SlidingPiece.new(:black,[0,7],self,:rook)
-  #   self[[1,0]] = Pawn.new(:black,[1,0],self,:pawn)
-  #   self[[1,1]] = Pawn.new(:black,[1,1],self,:pawn)
-  #   self[[1,2]] = Pawn.new(:black,[1,2],self,:pawn)
-  #   self[[1,3]] = Pawn.new(:black,[1,3],self,:pawn)
-  #   self[[1,4]] = Pawn.new(:black,[1,4],self,:pawn)
-  #   self[[1,5]] = Pawn.new(:black,[1,5],self,:pawn)
-  #   self[[1,6]] = Pawn.new(:black,[1,6],self,:pawn)
-  #   self[[1,7]] = Pawn.new(:black,[1,7],self,:pawn)
-  #
-  #   self[[7,0]] = SlidingPiece.new(:white,[7,0],self,:rook)
-  #   self[[7,1]] = SteppingPiece.new(:white,[7,1],self,:knight)
-  #   self[[7,2]] = SlidingPiece.new(:white,[7,2],self,:bishop)
-  #   self[[7,3]] = SlidingPiece.new(:white,[7,3],self,:queen)
-  #   self[[7,4]] = SteppingPiece.new(:white,[7,4],self,:king)
-  #   self[[7,5]] = SlidingPiece.new(:white,[7,5],self,:bishop)
-  #   self[[7,6]] = SteppingPiece.new(:white,[7,6],self,:knight)
-  #   self[[7,7]] = SlidingPiece.new(:white,[7,7],self,:rook)
-  #   self[[6,0]] = Pawn.new(:white,[6,0],self,:pawn)
-  #   self[[6,1]] = Pawn.new(:white,[6,1],self,:pawn)
-  #   self[[6,2]] = Pawn.new(:white,[6,2],self,:pawn)
-  #   self[[6,3]] = Pawn.new(:white,[6,3],self,:pawn)
-  #   self[[6,4]] = Pawn.new(:white,[6,4],self,:pawn)
-  #   self[[6,5]] = Pawn.new(:white,[6,5],self,:pawn)
-  #   self[[6,6]] = Pawn.new(:white,[6,6],self,:pawn)
-  #   self[[6,7]] = Pawn.new(:white,[6,7],self,:pawn)
-  #
-  #
-  # end
 
   def render
 
